bc1a012bb18c2affb5d3b8234fa66bfd813b690b
Chanh Duc Ngo <chanhduc@gmail.com>
2024-07-25 16:52:46 +0200

    Fix PolarProperties bean not found in BookControllerMvcTests

--- src/test/java/com/me/polarbookshop/catalogservice/web/BookControllerMvcTests.java
+++ src/test/java/com/me/polarbookshop/catalogservice/web/BookControllerMvcTests.java
@@ -1,9 +1,12 @@
 package com.me.polarbookshop.catalogservice.web;
 
+import com.me.polarbookshop.catalogservice.config.PolarProperties;
 import com.me.polarbookshop.catalogservice.domain.BookService;
 import com.me.polarbookshop.catalogservice.domain.exception.BookNotFoundException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
@@ -18,6 +21,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  * @created-date 25/07/2024
  */
 @WebMvcTest
+@EnableConfigurationProperties(value = PolarProperties.class)
 public class BookControllerMvcTests {
 
     @Autowired
