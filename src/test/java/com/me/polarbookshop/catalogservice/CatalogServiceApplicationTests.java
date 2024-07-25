00d9220bf68c0b37d6c016eae8fbc12cd279cb56
Chanh Duc Ngo <chanhduc@gmail.com>
2024-07-25 11:46:14 +0200

    Add Validation Test and Integeration test

--- src/test/java/com/me/polarbookshop/catalogservice/CatalogServiceApplicationTests.java
+++ src/test/java/com/me/polarbookshop/catalogservice/CatalogServiceApplicationTests.java
@@ -1,10 +1,36 @@
 package com.me.polarbookshop.catalogservice;
 
+import com.me.polarbookshop.catalogservice.domain.Book;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@SpringBootTest(
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
+)
 class CatalogServiceApplicationTests {
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @Test
+    void whenPostRequestValid_thenBookCreated() {
+        var expectedBook = new Book("1234567890", "Title", "Author", 9.99);
+
+        webTestClient.post()
+                .uri("/books")
+                .bodyValue(expectedBook)
+                .exchange()
+                .expectStatus().isCreated()
+                .expectBody(Book.class)
+                .value(actualBook -> {
+                    assertThat(actualBook).isNotNull();
+                    assertThat(actualBook.isbn()).isEqualTo(expectedBook.isbn());
+                });
+    }
 
     @Test
     void contextLoads() {
--- src/test/java/com/me/polarbookshop/catalogservice/domain/BookValidationTests.java
+++ src/test/java/com/me/polarbookshop/catalogservice/domain/BookValidationTests.java
@@ -1,5 +1,43 @@
-package com.me.polarbookshop.catalogservice.domain;/**
-* @author Chanh-Duc Ngo
-* @created-date 25/07/2024
-*/public class BookValidationTests {
+package com.me.polarbookshop.catalogservice.domain;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+/**
+ * @author Chanh-Duc Ngo
+ * @created-date 25/07/2024
+ */
+public class BookValidationTests {
+    private static Validator validator;
+
+    @BeforeAll
+    static void setup() {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+    }
+
+    @Test
+    void whenAllFieldsCorrect_thenValidationSucceeds() {
+        Book book = new Book("1234567890", "Title", "Author", 9.99);
+        Set<ConstraintViolation<Book>> violationSet = validator.validate(book);
+        assertThat(violationSet).isEmpty();
+    }
+
+    @Test
+    void whenIsbnDefinedButIncorrect_thenValidationFails() {
+        Book book = new Book("123456789", "Title", "Author", 9.99);
+        Set<ConstraintViolation<Book>> violationSet = validator.validate(book);
+        assertThat(violationSet).hasSize(1);
+        assertThat(violationSet.iterator().next().getMessage()).isEqualTo("The book ISBC must be a valid ISBN-10 or ISBN-13.");
+    }
+
 }
